6fa19fd53c638a8c3fcf314de1d6bb1983b1df97
Renaud <fournet.renaud@gmail.com>
2021-06-04 11:18:22 +0200

    moved menu on right

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -5,19 +5,19 @@ import logo from '../images/SouffleLogo/Digital/SVG/Souffle_Logo_BLANC_RGB.svg'
 const Menu = props => {
   return (
     <div>
-      <Navbar style={{ backgroundColor: '#1408D4' }}>
+      <Navbar style={{ backgroundColor: '#1408D4' }} light expand="mr-auto">
         <NavbarBrand>
           <img style={{ width: '30%' }} src={logo} alt="first" />
         </NavbarBrand>
-        <Nav className="mr-auto" navbar>
+        <Nav className="d-flex flex-row-reverse bd-highlight" navbar>
           <NavItem>
-            <NavLink style={{ color: 'white' }}>Accueil</NavLink>
+            <NavLink style={{ color: 'white', marginRight: '30px', marginLeft: '30px' }}>Accueil</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink style={{ color: 'white' }}>Notre Différence</NavLink>
+            <NavLink style={{ color: 'white', marginRight: '30px', marginLeft: '30px' }}>Notre Différence</NavLink>
           </NavItem>
           <NavItem>
-            <NavLink style={{ color: 'white' }}>Shop</NavLink>
+            <NavLink style={{ color: 'white', marginRight: '30px', marginLeft: '30px' }}>Shop</NavLink>
           </NavItem>
         </Nav>
       </Navbar>
